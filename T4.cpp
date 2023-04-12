bf4fad05ffd6ba5148b55a01d833835e118e3f6c
mosiyo <mkermani1383@gmail.com>
2023-04-13 00:50:35 +0330

    Build base form of main classes

---
+++ T4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int y,z;
+    cin>>y>>z;
+
+    cout<<y<<endl<<z;
+    return 0;
+}
